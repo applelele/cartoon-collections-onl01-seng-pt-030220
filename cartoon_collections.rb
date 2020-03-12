872607a4b407b39a85dc2cea309505401ca4bed3
YUKIKO IKEDA <applelele@gmail.com>
2020-03-12 04:02:21 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index|
+    puts "/#{index+1}. *#{dwarf}/"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|planeteer| "#{planeteer.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|planeteer| planeteer.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.any? {|food|cheese_types.include?(food)}
+    return array.find{|food| cheese_types.include?(food)}
+  else
+    return nil
+  end
 end
