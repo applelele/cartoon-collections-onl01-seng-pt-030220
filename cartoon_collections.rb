3b4695b3470f204f2e97af36761e7902b713038d
YUKIKO IKEDA <applelele@gmail.com>
2020-03-12 03:33:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?(cheese_types)
-    array.find?
+    array.find{|food| }
 end
